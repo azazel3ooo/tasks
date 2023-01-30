87441922b5127f67fc87b5c5ac20c28192d0cc3f
azazel3ooo <vasdanser@gmail.com>
2023-01-30 22:12:45 +0300

    init

--- tasks/ViewController.swift
+++ tasks/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var IncreaseButton: UIButton!
+    @IBOutlet weak var TextField: UILabel!
+    
+    private let startPhrase: String = "Значение счётчика:";
+    private var counter = 0;
+    
+    func buildPhrase() -> String {
+        return startPhrase + " " + String(counter)
+    }
+    
+    func updateText() {
+        TextField.text = buildPhrase()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        TextField.text = buildPhrase()
     }
 
-
+    
+    @IBAction func Increase() {
+        counter += 1;
+        updateText()
+    }
 }
 
